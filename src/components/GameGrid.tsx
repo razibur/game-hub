a0f7406579f343c20d3685cf7cfb802422f3f5f5
razibur <razibur@outlook.com>
2024-12-04 16:31:04 -0500

    Building game card

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,42 +1,22 @@
-import React, { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-import { Text } from "@chakra-ui/react";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
+import { SimpleGrid, Text } from "@chakra-ui/react";
+import useGames from "../hooks/useGames";
+import GameCard from "./GameCard";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<FetchGamesResponse>("/game")
-      .then((res) => {
-        if (res.data && Array.isArray(res.data.results)) {
-          setGames(res.data.results);
-        } else {
-          setError("Invalid data format");
-        }
-      })
-      .catch((err) => setError(err.message));
-  }, []);
+  const { games, error } = useGames();
 
   return (
     <>
       {error && <Text>{error}</Text>}
-      <ul>
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        padding="10px"
+        spacing={10}
+      >
         {games.map((game) => (
-          <li key={game.id}>{game.name}</li>
+          <GameCard key={game.id} game={game} />
         ))}
-      </ul>
+      </SimpleGrid>
     </>
   );
 };
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+export interface Game {
+  id: number;
+  name: string;
+  background_image: string;
+}
+
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
+
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    apiClient
+      .get<FetchGamesResponse>("/games", { signal: controller.signal})
+      .then((res) => {
+        if (res.data && Array.isArray(res.data.results)) {
+          setGames(res.data.results);
+        } else {
+          setError("Invalid data format");
+        }
+      })
+      .catch((err) => { 
+        if (err instanceof CanceledError) return;
+        setError(err.message)
+      });
+    
+    return () => controller.abort();
+  }, []);
+
+  return { games, error };
+}
+
+export default useGames;
